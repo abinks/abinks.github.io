057271ed27d845d7045839aba863a5f75310b5b2
abinks <47046183+abinks@users.noreply.github.com>
2021-03-12 11:29:48 -0600

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import { hot } from 'react-hot-loader/root';
+import React from 'react';
+
+
+function App(props) {
+
+    return (
+        <>
+            <div>
+                welcome to abinks.github.io
+            </div>
+            
+        </>
+    );
+}
+
+export default hot(App);
