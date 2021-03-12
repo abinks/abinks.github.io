a818cf34ded62912be597d06f3b04b840534bb31
abinks <allison.binkley@asurion.com>
2021-03-12 17:08:13 -0600

    initial commit

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,7 +6,7 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
     entry: "./src/index.js",
     output: {
-        path: path.join(__dirname, "/"),
+        path: path.join(__dirname, "/dist"),
         filename: "index_bundle.js"
     },
     module: {
